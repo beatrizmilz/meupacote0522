ed976911810316d66ff3e96fd60770104fbbf231
Beatriz Milz <milz.bea@gmail.com>
2022-05-30 19:37:25 -0300

    renomeia o arquivo da primeira_fun para media_vol

--- R/media_vol.R
+++ R/media_vol.R
@@ -11,6 +11,7 @@
 #'
 #' @examples media_vol("Cantareira")
 media_vol <- function(reservoir, year = 2022, month = 4) {
+
   "https://git.io/JOLeb" %>%
     readr::read_csv2() %>%
     dplyr::mutate(ano = lubridate::year(data), mes = lubridate::month(data)) %>%
